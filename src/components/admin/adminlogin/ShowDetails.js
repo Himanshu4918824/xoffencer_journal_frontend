b5d3855c3c0b239f3d89584e1b796ae072f5ad6c
kartikgupta666 <kartikgangil@gmail.com>
2025-09-05 13:52:36 +0530

    fixing loader

--- src/components/admin/adminlogin/ShowDetails.js
+++ src/components/admin/adminlogin/ShowDetails.js
@@ -11,8 +11,9 @@ import { useContext, useEffect, useState } from "react";
 import MainContext from "../../../context/maincontext";
 import { useParams } from "react-router-dom";
 
-export default function ShowDetails({ details }) {
+export default function ShowDetails() {
    const { id } = useParams();
+   const [loading, setLoading] = useState(false);
    const { getJournalDetail } = useContext(MainContext)
    const [data, setData] = useState({
       Abstract: "",
@@ -35,20 +36,28 @@ export default function ShowDetails({ details }) {
          return;
       }
       async function fetchdata() {
-         const data = await getJournalDetail(id)
-         setData({
-            Abstract: data[0].Abstract,
-            Author_Name: data[0].Author_Name,
-            Branch: data[0].Branch,
-            Created_at: data[0].Created_at,
-            Journal_Type: data[0].Journal_Type,
-            Second_Author_Guide_Name: data[0].Second_Author_Guide_Name,
-            Title_of_paper: data[0].Title_of_paper,
-            id: data[0].id,
-            subject: data[0].subject,
-            volume: data[0].Volume,
-            issue: data[0].Issue
-         })
+         setLoading(true)
+         try {
+            const data = await getJournalDetail(id)
+            setData({
+               Abstract: data[0].Abstract,
+               Author_Name: data[0].Author_Name,
+               Branch: data[0].Branch,
+               Created_at: data[0].Created_at,
+               Journal_Type: data[0].Journal_Type,
+               Second_Author_Guide_Name: data[0].Second_Author_Guide_Name,
+               Title_of_paper: data[0].Title_of_paper,
+               id: data[0].id,
+               subject: data[0].subject,
+               volume: data[0].Volume,
+               issue: data[0].Issue
+            })
+            setLoading(false)
+         }
+         catch (e) {
+            console.log(e)
+            setLoading(false)
+         }
       }
       fetchdata()
    }, [id, getJournalDetail])
@@ -89,62 +98,63 @@ export default function ShowDetails({ details }) {
             <Header />
          </div>
 
+         {loading ? <h1>Loading...</h1> : (<>
+            <div style={{ marginTop: 40, fontSize: 24, fontWeight: 'bold', letterSpacing: 0.5, width: '70%', height: '100%', textAlign: 'center', marginLeft: '12%' }}>{data.Title_of_paper}</div>
 
-         <div style={{ marginTop: 40, fontSize: 24, fontWeight: 'bold', letterSpacing: 0.5, width: '70%', height: '100%', textAlign: 'center', marginLeft: '12%' }}>{data.Title_of_paper}</div>
+            <div style={{ marginTop: 20, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
+               <b>Author(S):</b> {data.Author_Name} , {data.Second_Author_Guide_Name}
+            </div>
 
-         <div style={{ marginTop: 20, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
-            <b>Author(S):</b> {data.Author_Name} , {data.Second_Author_Guide_Name}
-         </div>
+            <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
+               <b>Publication #:</b> {data.id}
+            </div>
 
-         <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
-            <b>Publication #:</b> {data.id}
-         </div>
+            <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
+               <b>Date of Publication:</b> {new Date(data.Created_at).toLocaleDateString()}
+            </div>
 
-         <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
-            <b>Date of Publication:</b> {new Date(data.Created_at).toLocaleDateString()}
-         </div>
+            <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
+               <b>Country:</b> India
+            </div>
 
-         <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
-            <b>Country:</b> India
-         </div>
-
-         {/* <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
+            {/* <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
             <b>Pages:</b> 1-7
          </div> */}
 
-         <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
-            <b>Published In:</b> Volume {data.volume} Issue {data.issue} : {new Date(data.Created_at).getMonth()+1}-{new Date(data.Created_at).getFullYear()}
-         </div>
+            <div style={{ marginTop: 7, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
+               <b>Published In:</b> Volume {data.volume} Issue {data.issue} : {new Date(data.Created_at).getMonth() + 1}-{new Date(data.Created_at).getFullYear()}
+            </div>
 
-         <div style={{ marginTop: 8, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
-            <div style={{ fontWeight: 600, marginBottom: 5 }}>Abstract</div>
-            <div style={{ fontSize: 16 }}>{data.Abstract}</div>
-         </div>
+            <div style={{ marginTop: 8, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
+               <div style={{ fontWeight: 600, marginBottom: 5 }}>Abstract</div>
+               <div style={{ fontSize: 16 }}>{data.Abstract}</div>
+            </div>
 
-         <div style={{ marginTop: 20, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
-            <span style={{ fontWeight: 600, marginBottom: 5 }}>Keywords:</span> <span style={{ fontSize: 17 }}>Electronic Evidence, Indian Evidence Act, Judicial Reform, Digital Forensics, Legal Admissibility.</span>
-         </div>
+            <div style={{ marginTop: 20, fontWeight: 400, fontSize: ' 1.2rem', marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
+               <span style={{ fontWeight: 600, marginBottom: 5 }}>Keywords:</span> <span style={{ fontSize: 17 }}>Electronic Evidence, Indian Evidence Act, Judicial Reform, Digital Forensics, Legal Admissibility.</span>
+            </div>
 
 
 
-         <div style={{ textAlign: "center", marginTop: 40, marginBottom: 20 }}>
-            <Button variant="contained" color="primary" onClick={() => handleDownload(data.id)}>Download/View Paper PDF</Button>
-         </div>
+            <div style={{ textAlign: "center", marginTop: 40, marginBottom: 20 }}>
+               <Button variant="contained" color="primary" onClick={() => handleDownload(data.id)}>Download/View Paper PDF</Button>
+            </div>
 
-         <div style={{ marginTop: 50, fontWeight: 400, fontSize: 18, marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
-            Share this Article
+            <div style={{ marginTop: 50, fontWeight: 400, fontSize: 18, marginLeft: '7%', letterSpacing: 0.3, marginRight: '7%' }}>
+               Share this Article
 
-            <div style={{ marginTop: 5, cursor: 'pointer', fontSize: 20 }}>
-               <WhatsAppIcon style={{ color: '#25D366', fontSize: 30 }} />
-               <FacebookIcon style={{ color: '#1877F2', marginLeft: 5, fontSize: 30 }} />
-               <TwitterIcon style={{ color: '#1DA1F2', marginLeft: 5, fontSize: 30 }} />
-               <LinkedInIcon style={{ color: '#0077B5', marginLeft: 5, fontSize: 30 }} />
-               <TelegramIcon style={{ color: '#0088cc', marginLeft: 5, fontSize: 30 }} />
+               <div style={{ marginTop: 5, cursor: 'pointer', fontSize: 20 }}>
+                  <WhatsAppIcon style={{ color: '#25D366', fontSize: 30 }} />
+                  <FacebookIcon style={{ color: '#1877F2', marginLeft: 5, fontSize: 30 }} />
+                  <TwitterIcon style={{ color: '#1DA1F2', marginLeft: 5, fontSize: 30 }} />
+                  <LinkedInIcon style={{ color: '#0077B5', marginLeft: 5, fontSize: 30 }} />
+                  <TelegramIcon style={{ color: '#0088cc', marginLeft: 5, fontSize: 30 }} />
+
+               </div>
 
             </div>
 
-         </div>
-
+         </>)}
          <div style={{ marginTop: 10 }}>
             <Footer />
          </div>

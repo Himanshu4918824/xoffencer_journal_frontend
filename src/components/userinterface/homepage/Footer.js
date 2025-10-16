7bf8c9af54576355ffab21bcb815d6593be4ddee
kartikgupta666 <kartikgangil@gmail.com>
2025-10-16 18:51:04 +0530

    adding logo in footer

--- src/components/userinterface/homepage/Footer.js
+++ src/components/userinterface/homepage/Footer.js
@@ -20,37 +20,39 @@ export default function Footer() {
 
   const showImage = () => {
     return data.map((item, i) => (
-      <div key={i} style={{display: 'flex',justifyContent: 'center',alignItems: 'center',margin: matches ? '20px' : '10px',}}>
-        <img alt="" src={item} style={{ width: matches ? 200 : 120, height: 'auto'}} />
+      <div key={i} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: matches ? '20px' : '10px', }}>
+        <img alt="" src={item} style={{ width: matches ? 200 : 120, height: 'auto' }} />
       </div>
     ));
   };
 
   return (
-    <div style={{ background: 'lightgray', width: '100%',padding: '40px 20px', boxSizing: 'border-box'}}> 
-      <div style={{display: 'flex',flexDirection: matches ? 'row' : 'column',justifyContent: 'space-between',alignItems: matches ? 'flex-start' : 'center',marginBottom: '40px',}}> 
+    <div style={{ background: 'lightgray', width: '100%', padding: '40px 20px', boxSizing: 'border-box' }}>
+      <div style={{ display: 'flex', flexDirection: matches ? 'row' : 'column', justifyContent: 'space-between', alignItems: matches ? 'flex-start' : 'center', marginBottom: '40px', }}>
         <div style={{ marginBottom: matches ? '0' : '20px' }}>
           <div style={{ fontSize: 20, fontWeight: 'bold', marginBottom: '10px' }}>Quick Links</div>
           <ul style={{ listStyle: 'none', padding: 0, lineHeight: 1.8, cursor: 'pointer' }}>
-            <li onClick={()=>navigate('/nationaljournal')}>Current Issue</li>
-            <li onClick={()=>navigate('/team')}>Editorial Board</li>
-            <li onClick={()=>navigate('/#')}>Join Us Reviewer</li>
-            <li><Link to='https://drive.google.com/file/d/12MZM91kuFzN5g7t71MebTwrLKSZeso5s/view?usp=sharing' target='' style={{textDecoration:'none',color:'#000'}}>Privacy Policy</Link></li>
-            <li><Link to='https://drive.google.com/file/d/1ftgXASSgX9whcIK1-SylsrHDlIXwN_k2/view?usp=sharing' target='' style={{textDecoration:'none',color:'#000'}}>Disclaimer</Link></li>
-            <li><Link to='https://drive.google.com/file/d/1ss_eQPCUUTs7HqvGZucmdN5ltWZXaWmw/view?usp=sharing' target='' style={{textDecoration:'none',color:'#000'}}>Terms and Conditions</Link></li>
+            <li onClick={() => navigate('/nationaljournal')}>Current Issue</li>
+            <li onClick={() => navigate('/team')}>Editorial Board</li>
+            <li onClick={() => navigate('/#')}>Join Us Reviewer</li>
+            <li><Link to='https://drive.google.com/file/d/12MZM91kuFzN5g7t71MebTwrLKSZeso5s/view?usp=sharing' target='' style={{ textDecoration: 'none', color: '#000' }}>Privacy Policy</Link></li>
+            <li><Link to='https://drive.google.com/file/d/1ftgXASSgX9whcIK1-SylsrHDlIXwN_k2/view?usp=sharing' target='' style={{ textDecoration: 'none', color: '#000' }}>Disclaimer</Link></li>
+            <li><Link to='https://drive.google.com/file/d/1ss_eQPCUUTs7HqvGZucmdN5ltWZXaWmw/view?usp=sharing' target='' style={{ textDecoration: 'none', color: '#000' }}>Terms and Conditions</Link></li>
           </ul>
         </div>
 
         <div style={{ marginBottom: matches ? '0' : '20px' }}>
           <div style={{ fontSize: 20, fontWeight: 'bold', marginBottom: '10px' }}>More Links</div>
           <ul style={{ listStyle: 'none', padding: 0, lineHeight: 1.8, cursor: 'pointer' }}>
-            <li><Link to='http://Xoffencerpublication.in' style={{textDecoration:'none',color:'#000'}}>Book Publication</Link></li>
-            <li onClick={()=>navigate('/aboutus')}>About Us</li>
-            <li onClick={()=>navigate('/nationaljournal')}>National Publication</li>
+            <li><Link to='http://Xoffencerpublication.in' style={{ textDecoration: 'none', color: '#000' }}>Book Publication</Link></li>
+            <li onClick={() => navigate('/aboutus')}>About Us</li>
+            <li onClick={() => navigate('/nationaljournal')}>National Publication</li>
           </ul>
+
         </div>
 
-        <div style={{textAlign: 'center'}}>
+
+        <div style={{ textAlign: 'center' }}>
           <div style={{ fontSize: 20, fontWeight: 'bold', marginBottom: '10px' }}>Contact Us</div>
           <div style={{ fontSize: 14, lineHeight: 1.8 }}>
             <div>WhatsApp us: 9827646334</div>
@@ -66,10 +68,18 @@ export default function Footer() {
           </div>
         </div>
       </div>
-
+      <div >
+        <Link to="https://creovate-io.vercel.app/" style={{textDecoration: 'none' , color:'inherit'}} >
+          <div style={{ padding: 5, display: "flex", alignItems: "center", marginTop: 5, textAlign: 'center', marginBottom: 2, justifyContent: 'center', cursor: 'pointer' }}>
+            <img src="https://creovate-io.vercel.app/android-chrome-512x512.png" height={50} width={50} alt="" />
+            <h1 style={{ fontFamily: 'fantasy', letterSpacing: 1, textTransform: 'uppercase', fontWeight: "lighter" }}>CreovateIO</h1>
+          </div>
+        </Link>
+        <p style={{ textAlign: 'center', fontWeight: "bolder" }}>WE BUILD TRUST</p>
+      </div>
       <Divider style={{ marginBottom: '40px' }} />
 
-      <div style={{display: 'flex',flexWrap: 'wrap',justifyContent: 'center',alignItems: 'center',}}>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', }}>
         {showImage()}
       </div>
     </div>
